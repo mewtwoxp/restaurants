6de1ff688ee1080cb814f80766e2747bb465e012
mewtwoxp <33268131+mewtwoxp@users.noreply.github.com>
2021-02-20 03:25:30 -0400

    Design Register Page

---
+++ screens/account/Register.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import RegisterForm from '../../components/account/RegisterForm'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+export default function Register() {
+    return (
+        <KeyboardAwareScrollView style={styles.container}>
+            <Image
+                source={require("../../assets/restaurant-logo.png")}
+                resizeMode="contain"
+                style={styles.image}
+            /> 
+            
+            <RegisterForm/>
+        </KeyboardAwareScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    image: {
+        height: 150,
+        width: "100%",
+        marginBottom: 20
+    },
+    container: {
+        flex: 1,
+        marginTop: 30
+    }
+})
